0d1cb6b0eb0c50bb5a9d6336c838f0a5e3ec358f
Haranmes <soliman.harames15@gmail.com>
2023-05-22 19:16:25 +0200

    Added roles
    TO DO
    
    Add abilities

--- Assets/js/valorant_shop.js
+++ Assets/js/valorant_shop.js
@@ -3,6 +3,10 @@ let img = "";
 let header = "";
 let node;
 let element = "";
+let div = "";
+let table = "";
+let y = 0;
+let x = 0;
 const elements = document.getElementById("agents");
 window.addEventListener("load", async (event) => {
   document.getElementsByTagName("body")[0].style.backgroundColor = "black";
@@ -19,7 +23,9 @@ window.addEventListener("load", async (event) => {
         element = document.createElement("div");
         element.setAttribute("class", "agent");
         element.style.backgroundImage= `linear-gradient(#${data.backgroundGradientColors[0]}, #${data.backgroundGradientColors[3]})`
+        element.style.padding="20px"
         elements.appendChild(element);
+
         img = document.createElement("img");
         img.src = data.displayIconSmall;
         img.width = "112";
@@ -37,8 +43,36 @@ window.addEventListener("load", async (event) => {
         para.appendChild(node);
         element.appendChild(para);
         
+        div = document.createElement("div");
+        div.setAttribute("class", "role")
+        node = document.createTextNode("Role")
+        div.appendChild(node)
+        element.appendChild(div);
 
+        img = document.createElement("img")
+        img.setAttribute("id", "role-img")
+        img.src=data.role.displayIcon
+        img.width="20";
+        element.appendChild(img)
 
+        para = document.createElement("p")
+        para.setAttribute("id", "description")
+        para.style.paddingTop="5%"
+        node = document.createTextNode(data.role.description)
+        para.appendChild(node)
+        element.appendChild(para)
+
+        table = document.createElement("table")
+        table.setAttribute("class", "abilities")
+        x = table.insertRow();
+        for(let ability of data.abilities) {
+          y = x.insertCell(0)
+          header = document.createElement("td")
+          node = document.createTextNode(ability.slot)
+          header.appendChild(node)
+          y.appendChild(header)
+
+        }
       }
     }
   });
