266387508c6ccb73723ec85c72676670354f3329
Haranmes <soliman.harames15@gmail.com>
2023-05-23 20:50:13 +0200

    collapsible

--- Assets/js/valorant_shop.js
+++ Assets/js/valorant_shop.js
@@ -7,8 +7,12 @@ let div = "";
 let table = "";
 let y = 0;
 let x = 0;
+let yt;
 const elements = document.getElementById("agents");
-window.addEventListener("load", async (event) => {
+
+
+
+window.addEventListener("load", (event) => {
   document.getElementsByTagName("body")[0].style.backgroundColor = "black";
   const settings = {
     async: true,
@@ -20,60 +24,79 @@ window.addEventListener("load", async (event) => {
     console.log(response.data);
     for (let data of response.data) {
       if (data.isPlayableCharacter == true) {
+        //Creates the Parent div for Agent cards
         element = document.createElement("div");
         element.setAttribute("class", "agent");
-        element.style.backgroundImage= `linear-gradient(#${data.backgroundGradientColors[0]}, #${data.backgroundGradientColors[3]})`
-        element.style.padding="20px"
+        element.style.backgroundImage = `linear-gradient(#${data.backgroundGradientColors[0]}, #${data.backgroundGradientColors[3]})`;
+        element.style.padding = "20px";
         elements.appendChild(element);
 
+        //inserts Image of Agent
         img = document.createElement("img");
         img.src = data.displayIconSmall;
         img.width = "112";
         element.appendChild(img);
 
+        //Displays Name of Agent
         header = document.createElement("h1");
         header.setAttribute("id", "name");
         node = document.createTextNode(data.displayName);
         header.appendChild(node);
         element.appendChild(header);
 
+        //Displays description of Agent
         para = document.createElement("p");
         para.setAttribute("id", "description");
         node = document.createTextNode(data.description);
         para.appendChild(node);
         element.appendChild(para);
-        
+
+        //Role div
         div = document.createElement("div");
-        div.setAttribute("class", "role")
-        node = document.createTextNode("Role")
-        div.appendChild(node)
+        div.setAttribute("class", "role");
+        node = document.createTextNode("Role");
+        div.appendChild(node);
         element.appendChild(div);
 
-        img = document.createElement("img")
-        img.setAttribute("id", "role-img")
-        img.src=data.role.displayIcon
-        img.width="20";
-        element.appendChild(img)
+        //Icon of role
+        img = document.createElement("img");
+        img.setAttribute("class", "role-icon");
+        img.src = data.role.displayIcon;
+        img.width = "20";
+        element.appendChild(img);
 
-        para = document.createElement("p")
-        para.setAttribute("id", "description")
-        para.style.paddingTop="5%"
-        node = document.createTextNode(data.role.description)
-        para.appendChild(node)
-        element.appendChild(para)
+        //role description
+        para = document.createElement("p");
+        para.setAttribute("class", "tooltiptext");
+        para.style.paddingTop = "5%";
+        node = document.createTextNode(data.role.description);
+        para.appendChild(node);
+        element.appendChild(para);
 
-        table = document.createElement("table")
-        table.setAttribute("class", "abilities")
-        x = table.insertRow();
-        for(let ability of data.abilities) {
-          y = x.insertCell(0)
-          header = document.createElement("td")
-          node = document.createTextNode(ability.slot)
-          header.appendChild(node)
-          y.appendChild(header)
-
-        }
+        //youtube video embed
+        let search = data.displayName
+        let API_KEY = "AIzaSyDQRccpeIgVBH01edFJMHayLI_2njHPv5E"
+        videoSearch(API_KEY, search,1)
       }
     }
   });
 });
+
+function videoSearch(key, search, maxResults) {
+  $.get(`https://www.googleapis.com/youtube/v3/search?key=${key}&type=video&part=snippet&maxResult=${maxResults}&q=${search}`, function(response) {
+    console.log(data)
+  })
+}
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i of coll) {
+  i.addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "flex") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "flex";
+    }
+  });
+}
